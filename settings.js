8991cbe8e03ae9dd1d5d24e2b0291cf8f9b75dd6
Rashmarley <yusufrasheed20345@gmail.com>
2025-04-15 08:53:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Rashmarley",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '2349165460647', //Your number
+  botName: "Rashmarley v62",
+  botOwner: 'Rashmarley bot', // Your name
+  ownerNumber: '+2349165460647', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
